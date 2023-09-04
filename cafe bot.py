5b66be9bf486ffa5af08503a3c6800e49f5adfd1
kouragita <73515952+kouragita@users.noreply.github.com>
2023-09-04 12:25:48 +0300

    enquiring using input module
    i found it fun to play with code. expect more modifications to this script, thus more fun enjoy.

---
+++ cafe bot.py
@@ -0,0 +1,13 @@
+#bot name Bee
+#Bee @ yur service
+
+name = input('what is yur name?\n')
+
+print('Hi,'+ name, 'and welcome to coffee coffers  shop.')
+
+menu = "coffee, burger, sandwhich, hotmilk, sausage"
+
+print('what would yu like to order ' + name, menu +'?')
+order = input()
+
+print('welldone', name, order, 'it is, enjoy.')
