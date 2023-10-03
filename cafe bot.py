5c1ddd6cf0719f9503e6f35790460c41e03d72e6
kouragita <73515952+kouragita@users.noreply.github.com>
2023-10-03 08:36:21 +0300

    Update cafe bot.py

--- cafe bot.py
+++ cafe bot.py
@@ -3,11 +3,19 @@
 
 name = input('what is yur name?\n')
 
-print('Hi,'+ name, 'and welcome to coffee coffers  shop.')
+print('Hi,' + name, 'am Bee and welcome to coffers cafe.')
 
-menu = "coffee, burger, sandwhich, hotmilk, sausage"
+menu = "coffee, burger, sandwich, capuccino, sausage"
 
-print('what would yu like to order ' + name, menu +'?')
+print('what would yu like to order ' + name, menu + '?')
+
 order = input()
 
-print('welldone', name, order, 'it is, enjoy.')
+price = 8
+
+quantity = input(" How many would yu like?\n")
+
+total = price * int(quantity)
+
+print("well Thank YOU", name, "your total is $" + str(total))
+print('Have a blast', name, 'enjoy yur', order + 's.')
